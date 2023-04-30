89d513777631e59f2a3fb104d0fcbcc402082ee0
Brandon Roberts <brandontrob@gmail.com>
2023-04-30 04:21:09 -0700

    Added Python general echo

---
+++ roberts135.online/public_html/cgi-bin/py-general-echo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import os
+import sys
+
+# Print HTTP headers
+print("Cache-Control: no-cache")
+print("Content-type: text/html\r\n")
+# Print HTML document
+print("<html><head><title>Get Echo</title></head>\
+<body><h1 align=center>Get Echo</h1>\
+<hr/>")
+envvars = os.environ
+query_string = envvars['QUERY_STRING']
+request_body = sys.stdin.read()
+print("<p><b>Request Method:</b> " + envvars['REQUEST_METHOD'] + "</p>")
+print("<p><b>Query String:</b> " + envvars['SERVER_PROTOCOL'] + "</p><h2>Query:</h2><ul>")
+tokens = query_string.split("&")
+for token in tokens:
+    split_token = token.split("=")
+    print("<li><b>" + split_token[0] + ":</b> " + split_token[1] + "</li>")
+print("</ul><h2>Message Body:</h2><ul>")
+tokens = request_body.split("&")
+for token in tokens:
+    split_token = token.split("=")
+    print("<li><b>" + split_token[0] + ":</b> " + split_token[1] + "</li>")
+# Print HTML footer
+print("</ul></body></html>")
--- roberts135.online/public_html/cgi-bin/py-post-echo.py
+++ roberts135.online/public_html/cgi-bin/py-post-echo.py
@@ -5,11 +5,11 @@ import sys
 print("Cache-Control: no-cache")
 print("Content-type: text/html\r\n")
 # Print HTML document
-print("<html><head><title>Get Echo</title></head>\
-<body><h1 align=center>Get Echo</h1>\
+print("<html><head><title>Post Echo</title></head>\
+<body><h1 align=center>Post Echo</h1>\
 <hr/><ul>")
-query_string = sys.stdin.read()
-tokens = query_string.split("&")
+request_body = sys.stdin.read()
+tokens = request_body.split("&")
 for token in tokens:
     split_token = token.split("=")
     print("<li><b>" + split_token[0] + ":</b> " + split_token[1] + "</li>")
