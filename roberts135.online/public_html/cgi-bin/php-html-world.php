390dd991d08651e5ecce306e444a6c5bf577d0c6
Brandon Roberts <brandontrob@gmail.com>
2023-04-30 01:32:48 -0700

    Added php hello world html

---
+++ roberts135.online/public_html/cgi-bin/php-html-world.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" href="images/favicon.svg">
+  <title>Hello, PHP HTML World!</title>
+</head>
+
+<body>
+    <h1>Hello, PHP HTML World!</h1>
+    <hr />
+    <p>This page was generated with PHP.</p>
+    <p>This program was run at: 
+        <?php
+            print time();
+        ?>
+    </p>
+    <p>Your current IP Address is: 
+        <?php
+            print $_SERVER['REMOTE_ADDR']
+        ?>
+    </p>
+</body>
+
+</html>
